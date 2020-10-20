27faeaad4d34f48a8771c99586ce0c767330ab68
strinicka <71693176+strinicka@users.noreply.github.com>
2020-10-20 22:13:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Created by Manofsteel21
+
+
+
+function obrisi()
+{
+	var a=document.getElementById("txt");
+	a.value="";
+}
+function add(n)
+{
+	var a=document.getElementById("txt");
+	a.value=a.value+n;
+}
+
+function calculate()
+{
+	a=document.getElementById ("txt");
+	a.value=eval(a.value);
+}
+
+
+function nazad() {
+ var v =document.getElementById("txt").value; document.getElementById("txt").value = v.substr(0, v.length - 1); 
+}
+
